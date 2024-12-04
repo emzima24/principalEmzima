cf1101a061ccacb03973c7b9874c340ff52568f5
Emiliano Zapata <ezapata.bnc@gmail.com>
2024-12-04 14:29:00 -0300

    Crear carpeta y archivos de Java

---
+++ principalEmzima/java/PrimeraClase/Complementario.java
@@ -0,0 +1,11 @@
+public class Complementario {
+    public static void main(String[] args) {
+        String mensajeBienvenida = "Bienvenido a Java";
+        float temperaturaActual  = 19.0f;   // f es obligatoria para float
+        boolean isDatoLogico = true;  // is has o can se usan para expresar que contendra un boleano
+        
+        System.out.println(mensajeBienvenida + " No queda de otra... es lo que hay");
+        System.out.println("La temperatura actual es " + temperaturaActual + " °C ");
+        System.out.println("¿Es JAVA un lenguaje necesario pero mas pesado que el plomo? " + isDatoLogico);
+        }
+}
---
+++ principalEmzima/java/PrimeraClase/PrimerPrograma.java
@@ -0,0 +1,21 @@
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        final String SALUDO = "Hola mundo";
+        final int EDAD = 70;
+        final String MI_NOMBRE = "Emiliano";
+
+        boolean isVerdad = true;
+        String gestorCasosPrueba = "Testlink"; // Mayuscula a diferencia del resto
+        double escalaMinimo = 1.0; 
+        float escalaMaximo = 10.0f;   // f es obligatoria para float
+        char letra = 'm'; // comillas simples obligatorias
+        long numerote = 9223372036854775807L;  // L es obligatoria para float
+        
+        System.out.println(SALUDO);
+        System.out.println("La edad de " + MI_NOMBRE + " es " + EDAD);
+        System.out.println("¿Es " + gestorCasosPrueba + " un gestor de TC que no vale la pena utilizar? " + isVerdad);
+        System.out.println("En la escala del " + escalaMinimo + " al " + escalaMaximo + ", ¿qué tan malo es Testlink? " + (escalaMaximo * 1000));
+        System.out.println("Una letra que identifica la característica principal de " + gestorCasosPrueba + " es " + letra);        
+        System.out.println("Un número grandote " + numerote);        
+    }
+}
