6fa043eee21829cb87c4d40498872cf01709c1e0
Emiliano Zapata <ezapata.bnc@gmail.com>
2024-12-10 14:18:33 -0300

    Actualizar TerceraClase

--- principalEmzima/java/TerceraClase/CompVerificacionContraseña.java
+++ principalEmzima/java/TerceraClase/CompVerificacionContraseña.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class CompVerificacionContraseña {
-    
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            
+            System.out.print("Ingresa la contraseña: ");
+            String password = input.nextLine();
+            boolean isPassword = password.equals("1234");
+            if (isPassword) {
+                System.out.println("Acceso concedido");
+            } else {
+                System.out.println("Acceso denegado");
+            }
+            input.close();
+        }
 }
