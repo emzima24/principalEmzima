10a6c4b4e7e13b63d294f20cd6ef3df5d0bbf2b7
Milton <miltonericklun@gmail.com>
2024-12-09 21:52:40 -0300

    Agregado de clase Cart y modificacion de la funcion 'add_to_cart'

--- principalEmzima/pythonAutomatizacion/saucedemoproject/Features/pages/Description.py
+++ principalEmzima/pythonAutomatizacion/saucedemoproject/Features/pages/Description.py
@@ -1,9 +1,10 @@
 from imports import *  
-from configconstan import BrowserConfig, Cards 
+from configconstan import BrowserConfig
 from Features.pages.Catalogue import Catalogue
+from Features.pages.Cart import Cart
 
 
-class Description(Catalogue):
+class Description(Catalogue, Cart):
     def __init__(self, chrome_browser):
         """Inicializa el navegador en la instancia Description herendando de Catalogue que hereda de Login.
         Lo solicita para comenzar e incorporarlo en el resto de las funciones"""
@@ -14,25 +15,16 @@ class Description(Catalogue):
         if not self.logged_in:
             raise RuntimeError("Debes iniciar sesión primero usando la función login() Heredada de Catalogue que la heredo de Login.")
 
-    def add_to_cart(self, product_name):
-        """Añade un producto al carro usando su ID."""
-        product = Cards.products.get(product_name)
+    def add_to_cart_description(self):
+        """Añade un producto al carro desde la página Description"""
         self.verify_login()
         WebDriverWait(self.browser, BrowserConfig.TIMEOUTLOW).until(
-            EC.element_to_be_clickable((By.ID, product['id_add_cart']))
-        ).click()
-        return self
-    
-    def cart(self):
-        """Abre el carro de compras desde la barra superior."""
-        self.verify_login()
-        WebDriverWait(self.browser, BrowserConfig.TIMEOUTLOW).until(
-            EC.element_to_be_clickable((By.ID, 'shopping_cart_container'))
+            EC.element_to_be_clickable((By.ID, 'add-to-cart'))
         ).click()
         return self
 
     def back_to_products(self):
-        """Se devuelve a la página de catalogo."""
+        """Se devuelve a la página Catalogue"""
         self.verify_login()
         WebDriverWait(self.browser, BrowserConfig.TIMEOUTLOW).until(
             EC.element_to_be_clickable((By.ID, 'back-to-products'))
