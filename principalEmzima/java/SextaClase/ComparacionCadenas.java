e32c839e0aa8245e927621f38460c435e401ae00
Emiliano Zapata <ezapata.bnc@gmail.com>
2024-12-16 14:30:59 -0300

    Actualizaciones java clase 6

--- principalEmzima/java/SextaClase/ComparacionCadenas.java
+++ principalEmzima/java/SextaClase/ComparacionCadenas.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class ComparacionCadenas {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingresa la primera palabra: ");
+        String firstWord = input.nextLine();
+        System.out.print("Ingresa la segunda palabra: ");
+        String lastWord = input.nextLine();
+        String result = (firstWord.equals(lastWord)) ? "igual" : "diferente";
+        System.out.println(
+                "La palabra ingresada \"" + firstWord + "\" es " + result + "\na la palabra ingresada \"" + lastWord
+                        + "\"");
+        input.close();
+    }
 }
