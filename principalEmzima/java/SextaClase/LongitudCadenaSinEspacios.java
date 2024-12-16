3749027f615196a0c1b4a0cce128b85956c30c94
Emiliano Zapata <ezapata.bnc@gmail.com>
2024-12-16 13:04:33 -0300

    Actualizaciones java clase 6

--- principalEmzima/java/SextaClase/LongitudCadenaSinEspacios.java
+++ principalEmzima/java/SextaClase/LongitudCadenaSinEspacios.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class LongitudCadenaSinEspacios {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingresa ingresa un texto: ");
+        String inputText = input.nextLine();
+        String textWithoutSpaces = inputText.replace(" ", "");
+        System.out.println(
+                "La longitud del texto ingresado sin espacios \"" + inputText + "\" es " + textWithoutSpaces.length());
+        System.out.println("La longitud del texto ingresado \"" + inputText + "\" es " + inputText.length());
+        input.close();
+    }
 }
