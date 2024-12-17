4090076ae8197aa5278cf9998c0383174dd6529f
Emiliano Zapata <ezapata.bnc@gmail.com>
2024-12-17 10:39:22 -0300

    Actualizaciones java clase 7

--- principalEmzima/java/SeptimaClase/CalculoRaizCuadrada.java
+++ principalEmzima/java/SeptimaClase/CalculoRaizCuadrada.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class CalculoRaizCuadrada {
     public static void main(String[] args) {
-        
+        int number = 0;
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.print("Ingresa un número entero: ");
+            number = Integer.parseInt(input.nextLine());
+            if (number < 0) {
+                System.out.println("La raiz cuadarada de un número negativo es un número complejo ahora no es el tema");
+            } else {
+                System.out.println("La raíz cuadrada del número entero (" + number + ") es " + Math.sqrt(number));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Che boludo un número entero, no cuesta tanto.");
+        }
+        input.close();
     }
 }
