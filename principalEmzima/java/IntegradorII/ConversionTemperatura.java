ce4a88647e89b994a9f0b565a3d7754e335c19d4
Emiliano Zapata <ezapata.bnc@gmail.com>
2024-12-18 14:31:46 -0300

    Actualizacion java Integrador II

--- principalEmzima/java/IntegradorII/ConversionTemperatura.java
+++ principalEmzima/java/IntegradorII/ConversionTemperatura.java
@@ -14,7 +14,7 @@ public class ConversionTemperatura {
             System.out.println("Las siguientes opciones de la unidad de medida de la Temperatura: ");
             System.out.println("C) Celsius o Centigrado");
             System.out.println("F) Farenheit");
-            System.out.println("K) Kelvin");
+            System.out.println("K) Kelvin"); // adicional
             System.out.print("Ingresa aqui: ");
             String temperatureUnit = input.nextLine();
 
@@ -29,8 +29,8 @@ public class ConversionTemperatura {
                     temperatureConv = (temperature - 32) * 5 / 9;
                     temperatureConvLast = temperatureConv + 273.15;
                 }
-                case "K" -> {
-                    if (temperature < 0) {
+                case "K" -> {                                   // adicional
+                    if (temperature < 0) {                      
                         throw new IllegalArgumentException(
                                 "Che boludo una temperatura mayor que 0, no cuesta tanto.");
                     }
@@ -40,7 +40,7 @@ public class ConversionTemperatura {
                     temperatureConv = temperatureConvLast * 9 / 5 + 32;
                 }
                 default -> throw new IllegalArgumentException(
-                        "Che boludo una letra C o F para identificar las unidades de temperaturas\ntampoco es tan díficil");
+                        "Che boludo una letra C, F o K para identificar las unidades de temperaturas\ntampoco es tan díficil");
             }
             System.out.printf("Una Temperatura %.2f %s es equivalente a %.2f %s y %.2f %s%n", temperature, startUnit,
                         temperatureConv, mediumUnit, temperatureConvLast,lastUnit);
---
+++ principalEmzima/java/IntegradorII/ValidacionContrasenha.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ValidacionContrasenha {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String text = "", message = "La contraseña";
+        int count = 0;
+
+        System.out.print("Ingresa una contraseña para validarla: ");
+        String password = input.nextLine();
+        int minimunChars = password.length();
+        if (minimunChars < 8) {
+            count += 1;
+            text = count + ") Debe ser mayor o igual a 8 (solo colocaste " + minimunChars + ")\n";
+        } 
+        if (!password.matches(".*[A-Z].*")) {
+            count += 1;
+            text += count + ") Debe contener al menos 1 letra mayúscula\n";
+        } 
+        if (!password.matches(".*[a-z].*")) {
+            count += 1;
+            text += count + ") Debe contener al menos 1 letra minúscula\n";
+        } 
+        if (!password.matches(".*\\d.*")) {
+            count += 1;
+            text += count + ") Debe contener al menos 1 digito\n";
+        } 
+        if (!password.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) {
+            count += 1;
+            text += count + ") Debe contener al menos 1 caracter especial\n";
+        }
+        // adicional 
+        if (password.startsWith(" ") || password.endsWith(" ")) {
+            count += 1;
+            text += count + ") No debe comenzar o terminar con espacios\n";
+        }
+        if (count==0){
+            System.out.println(message.concat(" está bien wacho pero no te confies que te roban"));
+        } else {
+            System.out.println(message.concat(" ES INSEGURA ACA LAS OBSERVACIONES\n").concat(text));
+
+        }
+        input.close();
+    }
+}
