76cafe1280a0747a18829f0699b105fb4bd67346
Emiliano Zapata <ezapata.bnc@gmail.com>
2024-12-18 20:59:32 -0300

    Actualizacion java Integrador II

--- principalEmzima/java/IntegradorII/ValidacionContrasenhaDos.java
+++ principalEmzima/java/IntegradorII/ValidacionContrasenhaDos.java
@@ -2,44 +2,49 @@ import java.util.Scanner;
 
 public class ValidacionContrasenhaDos {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String text = "", message = "La contraseña";
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("-------------------------------------------------");
+        System.out.println("Ingrese una contrasena que al menos cuente con: ");
+        System.out.println(" - 8 caracteres.");
+        System.out.println(" - Al menos una Mayuscula y una Minuscula.");
+        System.out.println(" - Al menos un numero.");
+        System.out.println(" - Al menos un caracter especial.");
+        System.out.println("-------------------------------------------------");
+        System.out.print(" password > ");
+        String password = teclado.nextLine(), errorMessage = "La contrasena presenta los siguientes errores: \n";
         int count = 0;
 
-        System.out.print("Ingresa una contraseña para validarla: ");
-        String password = input.nextLine();
-        int minimunChars = password.length();
-        if (minimunChars < 8) {
-            count += 1;
-            text = count + ") Debe ser mayor o igual a 8 (solo colocaste " + minimunChars + ")\n";
-        } 
-        if (!password.matches(".*[A-Z].*")) {
-            count += 1;
-            text += count + ") Debe contener al menos 1 letra mayúscula\n";
-        } 
-        if (!password.matches(".*[a-z].*")) {
-            count += 1;
-            text += count + ") Debe contener al menos 1 letra minúscula\n";
-        } 
-        if (!password.matches(".*\\d.*")) {
-            count += 1;
-            text += count + ") Debe contener al menos 1 digito\n";
-        } 
-        if (!password.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) {
-            count += 1;
-            text += count + ") Debe contener al menos 1 caracter especial\n";
+        if (password.length() < 8) {
+            count++;
+            errorMessage += "[" + count + "] no tiene al menos 8 caracteres\n";
         }
-        // adicional 
-        if (password.startsWith(" ") || password.endsWith(" ")) {
-            count += 1;
-            text += count + ") No debe comenzar o terminar con espacios\n";
-        }
-        if (count==0){
-            System.out.println(message.concat(" está bien wacho pero no te confies que te roban"));
-        } else {
-            System.out.println(message.concat(" ES INSEGURA ACA LAS OBSERVACIONES\n").concat(text));
 
+        if (!password.matches(".*[a-z].*")) {
+            count++;
+            errorMessage += "[" + count + "] no tiene al menos una minuscula\n";
         }
-        input.close();
+
+        if (!password.matches(".*[A-Z].*")) {
+            count++;
+            errorMessage += "[" + count + "] no tiene al menos una Mayuscula\n";
+        }
+
+        if (!password.matches(".*\\d.*")) {
+            count++;
+            errorMessage += "[" + count + "] no tiene al menos un numero\n";
+        }
+
+        if (!password.matches(".*[^a-z A-Z 0-9].*")) {
+            count++;
+            errorMessage += "[" + count + "] no tiene al menos un caracter especial\n";
+        }
+
+        if (count == 0) {
+            errorMessage += "Excelente! Su nueva contrasena es: ************* (" + password + ")";
+        } else {
+            System.out.println(errorMessage);
+        }
+
+        teclado.close();
     }
 }
